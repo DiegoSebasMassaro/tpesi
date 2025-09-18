a700086e4ffb5245b8392bf857fe848a0cb302fe
ruandlv46-code <ruan.dlv.46@gmail.com>
2025-09-18 16:51:28 -0300

    add index

--- index.php
+++ index.php
@@ -1,25 +1,52 @@
 <?php require "partials/header.php" ?>
 
-<h1>BIENVENIDOS A LA CLASE ESI: "LOS FRIKIS"</h1>
-
-<form action="colores-def.php" method="POST">
-    <label for="valor">Eligir palabra</label>
-    <select name="valor" id="valor">
-        <option value="verde">Igualdad</option>
-        <option value="amarillo">Derecho</option>
-        <option value="azul">Diversidad</option>
-       
-
-    </select>
-    <input type="submit" value="Creditos">
+<div class="container text-center mt-5 text-success">
+    <div class="row">
+        <div class="col">
+            <h1>BIENVENIDOS A LA CLASE ESI: "LOS FRIKIS"</h1>
+        </div>
+    </div>
 
 
-    </select>
-    <input type="submit" value="GITHUB">
+    <div class="container text-center mt-5">
+        <div class="row">
+            <div class="col">
+                <form action="colores-def.php" method="POST">
+                    <label for="valor" >Eligir palabra</label>
+                    <select name="valor" id="valor">
+                        <option value="Igualdad">Igualdad</option>
+                        <option value="Derecho">Derecho</option>
+                        <option value="Diversidad">Diversidad</option>
 
-</form>
+                    </select>
+
+                    <input type="submit" value="ver mensaje" class= "btn btn-success">
 
 
-<?php require "partials/footer.php" ?>
+                </form>
+            </div>
+        </div>
+
+        <div class="container text-center mt-5">
+            <div class="row">
+                <div class="col">
+                    <!--       <input type="submit" value="Creditos"> -->
+
+                    <a href="creditos.php" class="mt-3 btn btn-success">Creditos</a>
+
+                </div>
+                <div class="col">
+<!--                     <input type="submit" value="GITHUB">
+ --> 
+                    <a href="GITHUB.php" class="mt-3 btn btn-success">GITHUB</a>
+
+                </div>
+            </div>
 
 
+
+
+
+
+
+            <?php require "partials/footer.php" ?>
