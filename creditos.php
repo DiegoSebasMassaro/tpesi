841f8ca3a9ddd0597daa90e6ca2dcf02a32def6d
DiegoSebasMassaro <diego.sebasmassaro@gmail.com>
2025-09-18 16:11:55 -0300

    fix creditos y header

--- creditos.php
+++ creditos.php
@@ -1,4 +1,18 @@
 
 
+<body>
 
+    <main>
+            <h1 class="text-center text-success">CREDITOS</h1>
+            <div class="d-flex justify-content-center">
+        <div class="d-flex justify-content-between col-9">
+<h2 class="text-success">Odila</h2>
+<h2 class="text-success">Ruan</h2>
+<h2 class="text-success">Diego</h2>
+        </div>
 
+    </div>
+    <div class="d-flex justify-content-center">
+        <a href="index.php" class="mt-3 btn btn-success">Inicio</a>
+</div>
+    </main>
